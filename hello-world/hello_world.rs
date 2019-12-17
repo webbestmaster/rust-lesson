4f04266be2c0e4bbdb052ca9d2053b16fb89eeed
WebBestMaster <web.best.master@gmail.com>
2019-12-17 17:34:09 +0300

    0.1 + 0.2

--- hello-world/hello_world.rs
+++ hello-world/hello_world.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello world!");
+
+    let x: f32 = 0.1;
+    let y: f32 = 0.2;
+    let sum: f32 = x + y;
+
+    println!("{}f32 + {}f32 = {}f32", x, y, sum);
+
+    let x: f64 = 0.1;
+    let y: f64 = 0.2;
+    let sum: f64 = x + y;
+
+    println!("{}f64 + {}f64 = {}f64", x, y, sum);
 }
