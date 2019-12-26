ce4e09f71a02efc73071c78cb9bd3d1731966ec5
WebBestMaster <web.best.master@gmail.com>
2019-12-26 16:41:50 +0300

    vector

---
+++ vector/src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn main() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    v.push(6);
+
+    let first = i32::from(2);
+
+//    v.push(6);
+
+    println!("{:?}", v);
+    println!("{}", first);
+
+//    let mut v2: Vec<Rectangle> = Vec::new();
+    let mut v2: Vec<Rectangle> = Vec::new();
+    v2.push(Rectangle {length: 22, width: 33});
+
+    let v2first = &v2[0];
+//    let v2sec = v2[0];
+
+    println!("{}", v2first.length);
+//    println!("{}", v2sec.length);
+
+}
