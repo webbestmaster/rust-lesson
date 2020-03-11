8b0fc649daca5b1008d59da1189f87fa3d8c3560
WebBestMaster <web.best.master@gmail.com>
2020-03-11 22:51:25 +0300

    in progress ...

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,14 +1,46 @@
 fn main() {
-    let mut x = String::from("the x");
+    let mut x: &str = "the x";
 
-
-    plus_one(& mut x);
+    plus_one(&mut x);
     println!("The value of x is: {}", x);
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    let mut string = String::from("Hello");
+    add_world(&mut string);
+    println!("The value of x is: {}", string);
+
+    let a: f32 = 0.1;
+    let b: f32 = 0.2;
+    println!("f32: 0.1 + 0.2 = {}", a + b);
+
+    let a: f64 = 0.1;
+    let b: f64 = 0.2;
+    println!("f64: 0.1 + 0.2 = {}", a + b);
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    let mut s = String::from("hello");
+    let r1 = &mut s;
+    println!("{}", r1);
+
+    let r2 = &mut s;
+    println!("{}", r2);
+
+//    println!("The value of x is: {}", s1); // will not works!!!
+    println!("The value of x is: {}", s2);
 }
 
-fn plus_one(x: & mut str) -> i32 {
-
+fn plus_one(x: &str) -> i32 {
     println!("The value of x is: {}", x);
 
     return 1;
+}
+
+fn add_world(string: &mut String) {
+    string.push_str(", world!");
 }
