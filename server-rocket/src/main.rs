bacf29002db927a8aa7a8178247279652248bd39
WebBestMaster <web.best.master@gmail.com>
2020-03-24 11:37:02 +0300

    in progress ...

---
+++ server-rocket/src/main.rs
@@ -0,0 +1,27 @@
+
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+use rocket::request::Form;
+use rocket::Data;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[derive(FromForm)]
+struct Task {
+    complete: bool,
+    description: String,
+}
+
+#[post("/file", data = "<task>")]
+fn new(task: Option<Form<Task>>)-> &'static str {
+    "Hello, world!11"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![hello, file]).launch();
+//    rocket::ignite().mount("/", routes![file]).launch();
+}
