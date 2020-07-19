1db32bd21e743bb2e7bdff529d8b6b864b31869a
dim <web.best.master@gmail.com>
2020-07-19 19:35:22 +0300

    gcd

--- paper-book/gcd/src/main.rs
+++ paper-book/gcd/src/main.rs
@@ -1,6 +1,31 @@
+use std::io::Write;
+use std::str::FromStr;
+use std::env;
+
+// TODO: research expect vs unwrap
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    println!("-----------");
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing arguments"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
     println!("{}", gcd(234, 567));
+    println!("Greatest common divisor of {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
