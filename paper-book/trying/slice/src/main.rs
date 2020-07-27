7633eabcf80b6632171eddcf64a79ed86267ac0b
dim <web.best.master@gmail.com>
2020-07-27 23:20:12 +0300

    p296-slice-item

--- paper-book/trying/slice/src/main.rs
+++ paper-book/trying/slice/src/main.rs
@@ -1,3 +1,9 @@
+#[derive(Debug)]
+struct MyStruct<'a> {
+    name: &'a str,
+    list: Vec<i32>,
+}
+
 fn count<T>(list: &[T]) -> i32 {
     let mut counter: i32 = 0;
 
@@ -15,4 +21,13 @@ fn main() {
     let list_length = count::<i32>(&int_list);
 
     println!("{}", list_length);
+
+    let mut my_struct = MyStruct {
+        name: "earl",
+        list: vec!(1, 2, 3),
+    };
+
+    my_struct.list.push(4);
+
+    println!("{:?}", my_struct);
 }
