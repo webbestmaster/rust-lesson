f850a4cf0da47205a1742c1eea38eda3f9fd47db
dim <web.best.master@gmail.com>
2020-08-22 16:44:02 +0300

    yandex contest - D - in progress...

--- yandex-contest/d-spiral/src/main.rs
+++ yandex-contest/d-spiral/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut list_length_str = String::new();
+
+    io::stdin().read_line(&mut list_length_str).expect("Wrong file, should contain several line with numbers");
+
+    let list_length = list_length_str.trim().parse::<i32>().expect("Wrong file, should contain several line with numbers");
+
+    println!("list_length {}", list_length);
 }
