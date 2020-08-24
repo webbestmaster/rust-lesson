2d4e3fea3a398d99f1d08cbeb9ba7448854637ae
dim <web.best.master@gmail.com>
2020-08-24 12:44:58 +0300

    in progress...

--- snake-3/src/main.rs
+++ snake-3/src/main.rs
@@ -14,6 +14,47 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use std::collections::LinkedList;
 use std::iter::FromIterator;
 
-fn main() {
+pub struct Game {
+    gl: GlGraphics,
+}
 
+
+impl Game {
+    fn render(&mut self, args: &RenderArgs) {
+        const green: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            graphics::clear(green, gl);
+        });
+    }
+}
+
+fn main() {
+    let opengl = OpenGL::V3_2;
+
+    const COLS: u32 = 30;
+    const ROWS: u32 = 20;
+    const SQUARE_WIDTH: u32 = 20;
+
+    let width = COLS * SQUARE_WIDTH;
+    let height = ROWS * SQUARE_WIDTH;
+
+    let mut window: GlutinWindow = WindowSettings::new("Snake Game", [width, height])
+        // .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game {
+        gl: GlGraphics::new(opengl)
+    };
+
+    let mut events = Events::new(EventSettings::new());
+
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r);
+            // println!("{:?}", r);
+        }
+    }
 }
