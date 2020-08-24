134306660e89b3f576b03a9d035e66923ee17676
dim <web.best.master@gmail.com>
2020-08-24 11:22:50 +0300

    in progress...

---
+++ snake-3/src/main.rs
@@ -0,0 +1,19 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+extern crate rand;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+
+use std::collections::LinkedList;
+use std::iter::FromIterator;
+
+fn main() {
+
+}
