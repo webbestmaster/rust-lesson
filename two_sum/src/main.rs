0c00d756aae5d428f91eba6297638f6b43971ebf
dim <web.best.master@gmail.com>
2024-01-01 17:10:11 +0100

    WIP

---
+++ two_sum/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let result: Vec<i32> = two_sum(vec![1, 2, 3, 4], 5);
+    assert_eq!(result, vec![1, 2]);
+
+    println!("Success!");
+}
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for first in nums.iter() {
+        pri
+    }
+
+
+    return vec![1, 2];
+}
