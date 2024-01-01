627e18a5afb9fe885ede4fe41b1f07343b21fe47
dim <web.best.master@gmail.com>
2024-01-01 17:33:43 +0100

    leetcode two-sum

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,15 +1,20 @@
+// https://leetcode.com/problems/two-sum/
 fn main() {
-    let result: Vec<i32> = two_sum(vec![1, 2, 3, 4], 5);
+    let result = two_sum(vec![1, 2, 3], 5);
+    println!("{:?}", result);
     assert_eq!(result, vec![1, 2]);
 
     println!("Success!");
 }
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for first in nums.iter() {
-        pri
+    for (index_a, first) in nums.iter().enumerate() {
+        for (index_b, second) in nums.iter().enumerate() {
+            if first + second == target && index_a != index_b {
+                return vec![index_a as i32, index_b as i32];
+            }
+        }
     }
 
-
-    return vec![1, 2];
+    return vec![];
 }
