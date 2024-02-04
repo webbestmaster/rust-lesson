d5f0057d1896dc9d71eaddbe3179b381dda8b662
dim <web.best.master@gmail.com>
2024-02-04 17:23:07 +0100

    refactor

--- a-star/src/main.rs
+++ a-star/src/main.rs
@@ -62,45 +62,10 @@ fn get_next_points(map: &Map, path_list: &PathList, point: &Point) -> Vec<Point>
     .collect::<Vec<Point>>()
 }
 
-fn main() {
-    let map: Map = Vec::from([
-        Vec::from(['.', '#', '.', '.', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '#', '.', '#', '.']),
-        Vec::from(['.', '.', '.', '#', '.']),
-        Vec::from(['.', '.', '.', '#', '.']),
-    ]);
-
-    let map2: Map = "1 2\n3 4"
-        .split_whitespace()
-        .collect::<Vec<&str>>()
-        .iter()
-        .map(|chunk| chunk.chars().collect::<Vec<char>>())
-        .collect::<Map>();
-
-    println!("map2 {:?}", map2);
-
-    // let point_begin = Point(2, 2);
-
-    let point_end = Point(4, 4);
-
-    // let mut new_points: Vec<Point> = Vec::new();
-
+fn get_short_path(map: &Map, point_begin: &Point, point_end: &Point) {
     let mut path_list: PathList = Vec::new();
 
-    let first_path: Path = vec![Point(0, 0)];
+    let first_path: Path = vec![point_begin.to_owned()];
     path_list.push(first_path);
 
     let mut new_path_list: PathList = Vec::new();
@@ -133,7 +98,7 @@ fn main() {
                     None => return false,
                 };
 
-                *last_point == point_end
+                last_point == point_end
             })
             .map(|data| -> Vec<Point> { data.to_owned() })
             .collect::<PathList>();
@@ -156,4 +121,102 @@ fn main() {
             path_list.push(path.to_owned());
         });
     }
+}
+
+fn main() {
+    let map: Map = Vec::from([
+        Vec::from(['.', '#', '.', '.', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '#', '.', '#', '.']),
+        Vec::from(['.', '.', '.', '#', '.']),
+        Vec::from(['.', '.', '.', '#', '.']),
+    ]);
+
+    let map2: Map = "1 2\n3 4"
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|chunk| chunk.chars().collect::<Vec<char>>())
+        .collect::<Map>();
+
+    println!("map2 {:?}", map2);
+
+    get_short_path(&map, &Point(0, 0), &Point(4, 4));
+
+    // let point_begin = Point(2, 2);
+
+    // let point_end = Point(4, 4);
+
+    // let mut new_points: Vec<Point> = Vec::new();
+
+    // let mut path_list: PathList = Vec::new();
+
+    // let first_path: Path = vec![Point(0, 0)];
+    // path_list.push(first_path);
+
+    // let mut new_path_list: PathList = Vec::new();
+
+    // loop {
+    //     new_path_list.clear();
+
+    //     // get all points from all pathes
+    //     path_list.iter().for_each(|path: &Vec<Point>| {
+    //         let last_point = match path.last() {
+    //             Some(point) => point,
+    //             None => return,
+    //         };
+
+    //         let new_points = get_next_points(&map, &path_list, last_point);
+
+    //         new_points.iter().for_each(|new_point| {
+    //             let mut cloned_path: Vec<Point> = path.clone();
+    //             cloned_path.push(new_point.to_owned());
+    //             new_path_list.push(cloned_path);
+    //         });
+    //     });
+
+    //     // check is reached
+    //     let reached_path_list: PathList = new_path_list
+    //         .iter()
+    //         .filter(|path| -> bool {
+    //             let last_point = match path.last() {
+    //                 Some(point) => point,
+    //                 None => return false,
+    //             };
+
+    //             *last_point == point_end
+    //         })
+    //         .map(|data| -> Vec<Point> { data.to_owned() })
+    //         .collect::<PathList>();
+
+    //     if !reached_path_list.is_empty() {
+    //         println!("reached_path_list {:?}", reached_path_list);
+    //         break;
+    //     }
+
+    //     if new_path_list.is_empty() {
+    //         println!("can not get new_path_list {:?}", new_path_list);
+    //         break;
+    //     }
+
+    //     path_list.clear();
+
+    //     // println!("new_path_list {}", new_path_list.len());
+
+    //     new_path_list.iter().for_each(|path| {
+    //         path_list.push(path.to_owned());
+    //     });
+    // }
 }
