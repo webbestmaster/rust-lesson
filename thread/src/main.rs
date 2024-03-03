b910f3aabb755927f63df39bdd00e3b6998607ea
dim <web.best.master@gmail.com>
2024-03-03 23:27:49 +0100

    rust to wasm

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,13 +1,44 @@
 use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
-    let v = vec![1, 2, 3];
+    let (tx, rx) = mpsc::channel::<String>();
+
+    let v: Vec<i32> = vec![1, 2, 3];
+    let link_v = v.to_owned();
 
     let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+
+
+        println!("Here's a vector: {:?}", link_v);
     });
+
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    // println!("{:?}", v);
 
 //    drop(v); // oh no!
 
-//    handle.join();
+   handle.join().ok();
 }
